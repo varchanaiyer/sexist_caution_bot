c4ae87b4437a20305574b403df0b9cf4a80cd1cd
archana iyer <varchanaiyer139@gmail.com>
2019-02-04 00:12:42 +0530

    initial bot

--- starterbot.py
+++ starterbot.py
@@ -2,6 +2,22 @@ import os
 import time
 import re
 from slackclient import SlackClient
+from keras.models import load_model
+import pickle
+import numpy as np
+import nltk
+from nltk.corpus import stopwords
+import os
+import re
+import sys
+import unicodedata
+from keras.preprocessing import sequence
+
+pred_list=['Insult', 'Not Insult']
+
+model=load_model('models/model1.h5')
+with open('tok.pkl', 'rb') as f:
+    tok = pickle.load(f)
 
 # instantiate Slack client
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
@@ -11,7 +27,40 @@ starterbot_id = None
 # constants
 RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
 
+def tokenize(sentences):
+    
+    tokens=[]
+    
+    for sentence in sentences:
+        sentence=nltk.word_tokenize(sentence)
+        tokens.append(sentence)
+    
+    return tokens
 
+def punc(sentences):
+    punctuation = dict.fromkeys([i for i in range(sys.maxunicode)
+                                 if unicodedata.category(chr(i)).startswith('P')])
+
+    new_sentences=[]
+    
+    for sentence in sentences:
+        sentence = [i.lower() for i in nltk.word_tokenize(sentence.translate(punctuation))]
+        sentence= ' '.join(sentence)
+        new_sentences.append(sentence)
+    
+    return new_sentences  
+
+def remove_stopwords(sentences):
+    stop_words=set(stopwords.words('english'))
+    
+    new_sentences=[]
+    
+    for sentence in sentences:
+        sentence = [word for word in sentence.lower().split() if word not in stop_words]
+        sentence = ' '.join(sentence)
+        new_sentences.append(sentence)
+    
+    return new_sentences
 
 def get_new_messages(slack_events):
     """
@@ -25,16 +74,38 @@ def get_new_messages(slack_events):
             return message, event["channel"]
     return None, None
 
+def predict(message):
+    comments=[message]
+    comments=punc(comments)
+    comments=remove_stopwords(comments)
+    comments=tokenize(comments)
+
+    sequences = tok.texts_to_sequences(comments)
+    max_len=50
+    sequences_matrix = sequence.pad_sequences(sequences,maxlen=max_len)
+    
+    pred=model.predict(sequences_matrix)
+    
+    if pred_list[np.argmax(pred)] == 'Insult':
+        return True
+    
+    return False
+
 
 def check_sexist(message, channel):
     """
         Finds a direct mention (a mention that is at the beginning) in message text
         and returns the user ID which was mentioned. If there is no direct mention, returns None
     """
+    message=message.lower()
+    sexist_list=[ "men will be men", "girls are like that",
+            'fuck', 'shit', 'as good as a man', 'like a man', 
+            'for a girl', 'smart for a girl', 'love of a woman', 'sex',
+            'men are better', 'girls cant do that']
 
-    sexist_list=[ "men will be men", "girls are like that"]
-
-    if message in sexist_list:
+    if any(ext in message for ext in sexist_list):
+        return True
+    elif predict(message):
         return True
     
     return False
